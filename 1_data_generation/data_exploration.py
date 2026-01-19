8f0ad4e3d805aedb499e7ce59528afc238c5919b
Antony Kiriinya <99920754+iamparody@users.noreply.github.com>
2026-01-19 08:36:35 +0300

    Delete 1_data_generation directory

--- 1_data_generation/data_exploration.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from sqlalchemy import create_engine
-
-engine = create_engine(
-    "postgresql+psycopg2://postgres:Iamparody_94@localhost:5432/analytics"
-)
-
-df = pd.read_sql("SELECT * FROM patient_clinical_timeseries LIMIT 10;", engine)
-print(df)
